fa34827b78416f7c24b89768d32884fca1650f5b
balakullayappa chinthapalle <balakullayappa.chinthapalle@serole.com>
2024-03-22 19:33:55 +0530

    first commit

---
+++ src/test/java/com/w2a/base/TestBase.java
@@ -0,0 +1,262 @@
+package com.w2a.base;
+
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+import java.lang.annotation.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.w2a.listeners.CustomListeners;
+import com.w2a.utilities.ExcelReader;
+import com.w2a.utilities.TestUtil;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	//again test2
+	/*
+	 * WebDriver - done Properties - done Logs - log4j jar, .log,
+	 * log4j.properties, Logger ExtentReports DB Excel Mail ReportNG,
+	 * ExtentReports Jenkins
+	 * 
+	 */
+
+	public static WebDriver driver;
+	public static Properties config = new Properties();
+	public static Properties OR = new Properties();
+	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger(TestBase.class);
+	public static ExcelReader excel = new ExcelReader(
+			System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+
+	public String browser="abc";
+
+	@BeforeSuite
+	public void setUp() throws InterruptedException {
+
+		if (driver == null) {
+			
+			PropertyConfigurator.configure(System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\log4j.properties");
+
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				config.load(fis);
+				log.debug("Config file loaded !!!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				OR.load(fis);
+				log.debug("OR file loaded !!!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			if(System.getenv("browser")!=null && !System.getenv("browser").isEmpty()){
+				
+				browser = System.getenv("browser");
+			}else{
+				
+				browser = config.getProperty("browser");
+				
+			}
+			
+			config.setProperty("browser", browser);
+			
+			
+			
+
+			if (config.getProperty("browser").equals("firefox")) {
+				System.setProperty("webdriver.gecko.driver", 
+				System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\geckodriver.exe");
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+
+			} else if (config.getProperty("browser").equals("Chrome")) {
+
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
+			System.out.println("Start");
+				WebDriverManager.chromedriver().setup();
+				System.out.println("Finish");
+				driver = new ChromeDriver();
+				System.out.println("Next Step");
+				log.debug("Chrome Launched !!!");
+			} else if (config.getProperty("browser").equals("ie")) {
+
+				System.setProperty("webdriver.ie.driver",
+						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+
+			}
+			Thread.sleep(10000);
+			driver.get(config.getProperty("testsiteurl"));
+			log.debug("Navigated to : " + config.getProperty("testsiteurl"));
+			driver.manage().window().maximize();
+			Thread.sleep(10000);
+		}
+
+	}
+
+	public void click(String locator) throws InterruptedException {
+
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_XPATH")) {
+			Thread.sleep(10000);
+			driver.findElement(By.xpath(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).click();
+		}
+		//CustomListeners.testReport.get().log(Status.INFO, "Clicking on : " + locator);
+	}
+
+	public void type(String locator, String value) throws InterruptedException {
+
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_XPATH")) {
+			driver.findElement(By.xpath(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).sendKeys(value);
+		}
+
+		//CustomListeners.testReport.get().log(Status.INFO, "Typing in : " + locator + " entered value as " + value);
+
+	}
+	
+	static WebElement dropdown;
+
+	public void select(String locator, String value) {
+
+		if (locator.endsWith("_CSS")) {
+			dropdown = driver.findElement(By.cssSelector(OR.getProperty(locator)));
+		} else if (locator.endsWith("_XPATH")) {
+			dropdown = driver.findElement(By.xpath(OR.getProperty(locator)));
+		} else if (locator.endsWith("_ID")) {
+			dropdown = driver.findElement(By.id(OR.getProperty(locator)));
+		}
+		
+		Select select = new Select(dropdown);
+		select.selectByVisibleText(value);
+
+		//CustomListeners.testReport.get().log(Status.INFO, "Selecting from dropdown : " + locator + " value as " + value);
+
+	}
+	
+	public void write(String result,Method m, int colNum, int rowNum) {
+		String excelFilePath = System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx";
+		try {
+		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+		Workbook workbook = WorkbookFactory.create(inputStream);
+		
+		Sheet sheet = workbook.getSheet(m.getName());
+		Cell cell2Update = sheet.getRow(rowNum).getCell(colNum);
+		cell2Update.setCellValue(result);
+		inputStream.close();
+		
+		FileOutputStream outputStream = new FileOutputStream(
+		System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+		workbook.write(outputStream);
+		workbook.close();
+		outputStream.close();
+		
+		} catch (Exception ex) {
+		ex.printStackTrace();
+		}
+		}
+
+
+	public boolean isElementPresent(By by) {
+
+		try {
+
+			driver.findElement(by);
+			return true;
+
+		} catch (NoSuchElementException e) {
+
+			return false;
+
+		}
+
+	}
+	public static void verifyEquals(String expected, String actual) throws IOException {
+
+		try {
+
+			Assert.assertEquals(actual, expected);
+
+		} catch (Throwable t) {
+
+			TestUtil.captureScreenshot();
+			// ReportNG
+			Reporter.log("<br>" + "Verification failure : " + t.getMessage() + "<br>");
+			Reporter.log("<a target=\"_blank\" href=" + TestUtil.screenshotName + "><img src=" + TestUtil.screenshotName
+					+ " height=200 width=200></img></a>");
+			Reporter.log("<br>");
+			Reporter.log("<br>");
+			// Extent Reports
+			CustomListeners.testReport.get().log(Status.FAIL, " Verification failed with exception : " + t.getMessage());
+			CustomListeners.testReport.get().fail("<b>" + "<font color=" + "red>" + "Screenshot of failure" + "</font>" + "</b>",
+					MediaEntityBuilder.createScreenCaptureFromPath(TestUtil.screenshotName)
+					.build());
+
+		}
+
+	}
+	
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+		log.debug("test execution completed !!!");
+	}
+}
---
+++ src/test/java/com/w2a/testcases/MMVTest.java
@@ -0,0 +1,181 @@
+package com.w2a.testcases;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Hashtable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestResult;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.w2a.base.TestBase;
+import com.w2a.utilities.TestUtil;
+
+public class MMVTest extends TestBase {
+	
+	public ExtentHtmlReporter htmlReporter;
+	public ExtentReports extent;
+	public ExtentTest test;
+	private int rowNum = 1;
+
+	@BeforeTest
+	public void setReport() {
+
+		htmlReporter = new ExtentHtmlReporter("./reports/MMVextent.html");
+
+		htmlReporter.config().setEncoding("utf-8");
+		htmlReporter.config().setDocumentTitle("ASV Automation Reports");
+		htmlReporter.config().setReportName("ASV Automation Test Results");
+		htmlReporter.config().setTheme(Theme.STANDARD);
+
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		extent.setSystemInfo("Automation Tester", "Bala Kullayappa");
+		extent.setSystemInfo("Orgainzation", "Serole Technologies");
+		//extent.setSystemInfo("Build No", "W2A-1234");
+
+
+	}
+
+	@AfterTest
+	public void endReport() {
+
+		extent.flush();
+	}
+	
+	
+	@Test(dataProviderClass=TestUtil.class,dataProvider="dp")
+	public void mMVTest(Hashtable<String,String> data, Method m) throws InterruptedException{
+		test = extent.createTest("MMV Test");
+		if (!(TestUtil.isTestRunnable("mMVTest", excel))) {
+
+			throw new SkipException("Skipping the test " + "mMVTest".toUpperCase() + "as the Run mode is NO");
+		}
+		if(data.get("Runmode").equalsIgnoreCase("N")){
+			throw new SkipException("Skipping the test case " + "mMVTest".toUpperCase() + " as the Run mode is NO");
+		
+		}
+		Thread.sleep(10000);
+		click("Login_XPATH");
+		type("Email_ID",data.get("Email"));
+		type("Password_ID",data.get("Password"));
+		click("SignIn_XPATH");
+		click("Home_XPATH");
+		click("MMV_XPATH");
+		select("Make_CSS", data.get("Make"));
+		Actions action = new Actions(driver);
+		Thread.sleep(2000);
+        action.sendKeys(Keys.TAB).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ARROW_DOWN).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.TAB).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ARROW_DOWN).perform();
+        Thread.sleep(2000);
+        action.sendKeys(Keys.ENTER).perform();
+		click("MMVSearch_XPATH");
+	    click("Vehicledetails_XPATH");
+	    click("FindParts_XPATH");
+	    if(data.get("Default Category").equalsIgnoreCase("ENGINE")) {
+	    	 click("Cat_XPATH");
+	    	 click("SubCat_XPATH");
+	    	 System.out.println("Selected subcategory");
+	    }
+	    click("Cart_XPATH");
+		click("Shopcart_XPATH");
+		click("Checkout_XPATH");
+		Thread.sleep(10000);
+		click("FlatRate_XPATH");
+		click("Next_XPATH");
+		click("Cash_XPATH");
+		click("PlaceOrder_XPATH");
+		Thread.sleep(10000);
+		System.out.println("Order Confirmed");
+		WebElement Premium = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/table/tfoot/tr[3]/td[2]"));
+		String TotalPremium = Premium.getText();
+		WebElement element = driver.findElement(By.cssSelector(
+				"body > div:nth-child(9) > main:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2)"));
+		String OrderNum = element.getText();
+		write(TotalPremium, m, 11, rowNum);
+		write(OrderNum, m, 12, rowNum);
+		rowNum++;
+		Thread.sleep(10000);
+		click("MyAccount_XPATH");
+		click("Logout_XPATH");
+		Thread.sleep(10000);
+     
+	}
+
+@AfterMethod
+public void tearDown(ITestResult result) {
+
+	if (result.getStatus() == ITestResult.FAILURE) {
+		
+		
+		String excepionMessage = Arrays.toString(result.getThrowable().getStackTrace());
+		test.fail("<details>" + "<summary>" + "<b>" + "<font color=" + "red>" + "Exception Occured:Click to see"
+				+ "</font>" + "</b >" + "</summary>" + excepionMessage.replaceAll(",", "<br>") + "</details>"
+				+ " \n");
+		
+		try {
+
+			TestUtil.captureScreenshot();
+			test.fail("<b>" + "<font color=" + "red>" + "Screenshot of failure" + "</font>" + "</b>",
+					MediaEntityBuilder.createScreenCaptureFromPath(TestUtil.screenshotName)
+							.build());
+		} catch (IOException e) {
+
+		}
+		String failureLogg = "TEST CASE FAILED";
+		Markup m = MarkupHelper.createLabel(failureLogg, ExtentColor.RED);
+		test.log(Status.FAIL, m);
+		
+		
+
+	} else if (result.getStatus() == ITestResult.SKIP) {
+
+		String methodName = result.getMethod().getMethodName();
+
+		String logText = "<b>" + "TEST CASE: - " + methodName.toUpperCase() + "  SKIPPED" + "</b>";
+
+		Markup m = MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
+		test.skip(m);
+
+	} else if (result.getStatus() == ITestResult.SUCCESS) {
+
+		String methodName = result.getMethod().getMethodName();
+
+		String logText = "<b>" + "TEST CASE: - " + methodName.toUpperCase() + "  PASSED" + "</b>";
+
+		Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
+		test.pass(m);
+
+	}
+
+}
+}
